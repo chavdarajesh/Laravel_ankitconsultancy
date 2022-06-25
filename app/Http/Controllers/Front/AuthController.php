c6e4815738955ac15ba3cb5a822e90b2d3b6fd99
rajesh_chavda <85052254+chavdarajesh@users.noreply.github.com>
2022-06-25 21:36:07 +0530

    mail use

--- app/Http/Controllers/Front/AuthController.php
+++ app/Http/Controllers/Front/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\Front\ForgotPassword;
 use App\Mail\Front\OTPVerification;
 
