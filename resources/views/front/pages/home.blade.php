f63d482828cb90fd2cce29b7b39fd78bcfb3d936
rajesh_chavda <85052254+chavdarajesh@users.noreply.github.com>
2022-06-29 20:26:21 +0530

    Update home.blade.php

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -69,8 +69,8 @@
     @endphp
 
     <section id="hero" class="hero d-flex align-items-center bg-video-wrap">
-        <video  loop muted autoplay>
-            <source src="{{asset('assets/front/videos/homepage/3943971.mp4')}}" type="video/mp4">
+        <video src="{{asset('assets/front/videos/homepage/3943971.mp4')}}" loop muted autoplay>
+            {{-- <source src="{{asset('assets/front/videos/homepage/3943971.mp4')}}" type="video/mp4"> --}}
                 Your browser does not support the video tag.
         </video>
         <div class="overlay">
